d57bbe2956cc107354a243bc9b14d5d80055ddb3
Soumya <sswaroop@thoughtworks.com>
2018-09-12 10:21:48 +0530

    book my show

--- bookmyshow.js
+++ bookmyshow.js
@@ -2,27 +2,27 @@ const { browser, openBrowser, goto, click,link,image } = require('taiko');
 
 (async () => {
     try {
-        await openBrowser({headless:false});
+        await openBrowser();
         await goto("https://in.bookmyshow.com/bengaluru");
 
         await click("Bengaluru");
         await write('NCR', into(await inputField({"placeholder":"Search for your city"})));
         await click("NCR");
         await click("View All");
-        await click(link("English"))
+        await click(link("English"),below('Select Language'))
         await click(image(below(link("Now Showing"))))
-        //await click(link("Book Tickets"))
-        await waitFor("5000")
+        await click(link("Book Tickets"))
 
         await click(link({"class":"__showtime-link"}))
-        await click("Accept")        
-        await click(link(toRightOf("D")))
+        await click("Accept")
+
         await click("Select Seats")
+        await click(link(toRightOf("D")))
 
         // await click(link(contains("Pay ")))
         // await click(radioButton(toLeftOf(contains("M-Ticket"))))
-        await waitFor("5000")
     } catch (e) {
+        await screenshot()
         console.error(e);
     } finally {
         await closeBrowser();
